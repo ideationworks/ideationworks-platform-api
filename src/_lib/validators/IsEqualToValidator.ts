65e98e1f12275ed543b8ecb3da2e992e73030126
jaime-franco <jaime@streamingplatformcorp.com>
2020-05-29 15:16:50 -0600

    :construction: IDEATE-2 Update user register validations
    Add IsEqualTo validation and update user register validations

---
+++ src/_lib/validators/IsEqualToValidator.ts
@@ -0,0 +1,27 @@
+import { registerDecorator, ValidationOptions, ValidationArguments } from "class-validator";
+/**
+ * 
+ * @param property name of the property to compare
+ * @param ValidationOptions validation options object
+ */
+export function IsEqualTo(property: string, ValidationOptions?: ValidationOptions) {
+    return function (object: Object, propertyName: string) {
+        registerDecorator({
+            name: 'isEqualTo',
+            target: object.constructor,
+            propertyName: propertyName,
+            constraints: [property],
+            options: {
+                message: `${property} must be equal to ${propertyName}`,
+                ...ValidationOptions
+            },
+            validator: {
+                validate(value: any, args: ValidationArguments) {
+                    const [realtedPropertyName] = args.constraints;
+                    const realtedValue = (args.object as any)[realtedPropertyName];
+                    return realtedValue === value;
+                }
+            }
+        });
+    };
+}
--- src/users/UserRegister.ts
+++ src/users/UserRegister.ts
@@ -1,7 +1,23 @@
-import { ApiProperty }     from '@nestjs/swagger';
-import { IsEmail, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, Length, IsOptional, IsString, IsDefined} from 'class-validator';
+import { IsEqualTo } from '../_lib/validators/IsEqualToValidator';
 
 export class UserRegister {
+
+    @ApiProperty()
+    @IsOptional()
+    @IsString()
+    public firstName: string;
+
+    @ApiProperty()
+    @IsOptional()
+    @IsString()
+    public lastName: string;
+
+    @ApiProperty()
+    @IsDefined()
+    @IsString()
+    public displayName: string;
 
     @ApiProperty()
     @IsEmail()
@@ -9,6 +25,14 @@ export class UserRegister {
 
     @ApiProperty()
     @Length(8, 255)
+    @IsString()
     public password: string;
 
+    @ApiProperty()
+    @Length(8, 255)
+    @IsString()
+    @IsEqualTo('password')
+    public confirmPassword: string;
+
 }
+
