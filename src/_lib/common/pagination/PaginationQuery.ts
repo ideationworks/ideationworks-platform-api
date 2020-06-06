c031af4a5d005bdc587a704d87c1e1739ebe0e42
jaime-franco <jaime@streamingplatformcorp.com>
2020-06-05 20:01:22 -0600

    :recycle: IDEATE-5 Refactor Query Filters
    Refactor query filter and add relations filter

--- src/_lib/common/pagination/PaginationQuery.ts
+++ src/_lib/common/pagination/PaginationQuery.ts
@@ -1,10 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsOptional } from "class-validator";
 import { Transform } from "class-transformer";
+import { FilterQuery } from "../queryFilter/FilterQuery";
+import { FindManyOptions } from "typeorm";
 /**
  * Query params use for pagination
  */
-export class PaginationQuery {
+export class PaginationQuery<T> extends FilterQuery<T> {
 
     @ApiProperty()
     @IsNumber()
@@ -17,5 +19,24 @@ export class PaginationQuery {
     @IsOptional()
     @Transform(value => (value && parseInt(value, 10)))
     size: number = 20;
+    
+    /**
+     * 
+     * Use to get findManyOptions use in typeorm queries
+     * @param options parameters use to determinate permisions for the query
+     */
+    getFindManyOptions(options?: QueryFilterOptions<T>): FindManyOptions<T> {
+
+        const query: FindManyOptions<T> = {
+
+            skip: this.offset,
+            take: this.size,
+            ...this.getFindOneOptions(options)
+
+        };
+
+        return query;
+            
+    }
 
 }
